17bee7e85907843a0968c624047d6617252e3c17
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-09-04 03:02:47 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from "./components/Menu.jsx";
 
 function App() {
   return (
-    <>
+      <Router>
+        <Toaster />
+        <Menu />
+        <Routes>
 
-    </>
-  )
+        </Routes>
+      </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,55 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import homeButton from '/assets/img/Menu/Buttons/home-button.png';
+import inviteButton from '/assets/img/Menu/Buttons/invite-button.png';
+import tasksButton from '/assets/img/Menu/Buttons/tasks-button.png';
+import leaderboardButton from '/assets/img/Menu/Buttons/leaderboard-button.png';
+import homeButtonClicked from '/assets/img/Menu/ButtonsClicked/home-button.png';
+import inviteButtonClicked from '/assets/img/Menu/ButtonsClicked/invite-button.png';
+import tasksButtonClicked from '/assets/img/Menu/ButtonsClicked/tasks-button.png';
+import leaderboardButtonClicked from '/assets/img/Menu/ButtonsClicked/leaderboard-button.png';
+
+const Menu = () => {
+    const navigate = useNavigate();
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+    const location = useLocation();
+
+    const handleNavigation = (path) => {
+        if (navigator.vibrate) {
+            navigator.vibrate(100);
+        } else {
+            console.log('Vibration API not supported');
+        }
+        navigate(path);
+    };
+
+    return (
+        <div className="fixed bottom-5 flex justify-around items-center w-full z-20">
+            <img
+                src={location.pathname === '/' ? homeButtonClicked : homeButton}
+                alt="Home"
+                className="menu-button cursor-pointer"
+                onClick={() => handleNavigation('/')}
+            />
+            <img
+                src={location.pathname === '/invite' ? inviteButtonClicked : inviteButton}
+                alt="Invite"
+                className="menu-button cursor-pointer"
+                onClick={() => handleNavigation('/invite')}
+            />
+            <img
+                src={location.pathname === '/tasks' ? tasksButtonClicked : tasksButton}
+                alt="Tasks"
+                className="menu-button cursor-pointer"
+                onClick={() => handleNavigation('/tasks')}
+            />
+            <img
+                src={location.pathname === '/leaderboard' ? leaderboardButtonClicked : leaderboardButton}
+                alt="Leaderboard"
+                className="menu-button cursor-pointer"
+                onClick={() => handleNavigation('/leaderboard')}
+            />
+        </div>
+    );
+};
+
+export default Menu;
